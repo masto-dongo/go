f068b2def66cde57140b7e2a4f7974a8054cb44a
kibigo! <marrus-sh@users.noreply.github.com>
2017-10-17 03:45:13 -0700

    Pre-Alpha Fixes 3D

--- components/start/index.js
+++ components/start/index.js
@@ -37,7 +37,7 @@ const messages = defineMessages({
     defaultMessage: 'Compose',
     id: 'start.drawer',
   },
-  compose: {
+  courier: {
     defaultMessage: 'Notifications',
     id: 'start.courier',
   },
