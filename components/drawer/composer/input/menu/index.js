bdab34e7194e47f35dc67d9c9fb625e380d03107
kibigo! <marrus-sh@users.noreply.github.com>
2017-10-18 02:48:08 -0700

    Pre-Alpha Fixes 63

--- components/drawer/composer/input/menu/index.js
+++ components/drawer/composer/input/menu/index.js
@@ -25,7 +25,7 @@ const messages = defineMessages({
     defaultMessage: 'Upload media',
     id: 'composer.attach',
   },
-  attach: {
+  close: {
     defaultMessage: 'Close',
     id: 'composer.close',
   },
