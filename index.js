d4100bbedbcf37528ca792593f7e64ebcc6d03b2
kibigo! <marrus-sh@users.noreply.github.com>
2018-01-06 21:27:58 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+//  EMOJIFY
+//  =======
+
+//  Imports.
+import Emoji from './emoji';
+import Emojifier from './emojifier';
+import EmojiData from './emoji_data';
+
+//  Named exports.
+export {
+  Emoji,
+  Emojifier,
+  EmojiData,
+};
